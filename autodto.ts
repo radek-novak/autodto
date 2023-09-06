0d0ea1f776630864e8c25b429dfb2ebe9b7c2137
Radek Novak <radek.novak.cze@gmail.com>
2023-09-06 18:56:57 +0200

    extract type

--- autodto.ts
+++ autodto.ts
@@ -6,44 +6,48 @@ function createProgram(filePath: string) {
 }
 
 const filePath = "./tests/simple/oneline-1.ts";
-const program = createProgram(filePath);
-// const sourcefiles = program.getSourceFiles();
-function visitNode(
-  node: ts.Node,
-  checker: ts.TypeChecker,
-  fileText: string,
-  isCorrectArg = false
-) {
-  let isCorrect = isCorrectArg;
 
-  ts.forEachLeadingCommentRange(fileText, node.getFullStart(), (pos, end) => {
-    const comment = fileText.substring(pos, end);
-    isCorrect = comment.includes("@autodto");
-  });
-  if (isCorrect && ts.isVariableDeclaration(node)) {
-    const type = checker.getTypeAtLocation(node);
-    const typeString = checker.typeToString(type);
+function extractTypes(filePath: string) {
+  const program = createProgram(filePath);
+  function visitNode(
+    node: ts.Node,
+    checker: ts.TypeChecker,
+    fileText: string,
+    isCorrectArg = false
+  ) {
+    let isCorrect = isCorrectArg;
 
-    console.log(node.pos, node.end, node.getText(), typeString);
+    ts.forEachLeadingCommentRange(fileText, node.getFullStart(), (pos, end) => {
+      const comment = fileText.substring(pos, end);
+      isCorrect = comment.includes("@autodto");
+    });
+    if (isCorrect && ts.isVariableDeclaration(node)) {
+      const type = checker.getTypeAtLocation(node);
+      const typeString = checker.typeToString(type);
+
+      console.log(node.pos, node.end, node.getText(), typeString);
+    }
+
+    ts.forEachChild(
+      node,
+      (child) => visitNode(child, checker, fileText, isCorrect),
+      (children) =>
+        children.forEach((child) =>
+          visitNode(child, checker, fileText, isCorrect)
+        )
+    );
   }
 
-  ts.forEachChild(
-    node,
-    (child) => visitNode(child, checker, fileText, isCorrect),
-    (children) =>
-      children.forEach((child) =>
-        visitNode(child, checker, fileText, isCorrect)
-      )
-  );
+  // existing code
+  const sourcefiles = program.getSourceFiles();
+  const checker = program.getTypeChecker();
+
+  sourcefiles
+    .filter((sourceFile) => !sourceFile.isDeclarationFile)
+    .forEach((sourceFile) => {
+      const fileText = sourceFile.getFullText();
+      sourceFile.forEachChild((node) => visitNode(node, checker, fileText));
+    });
 }
 
-// existing code
-const sourcefiles = program.getSourceFiles();
-const checker = program.getTypeChecker();
-
-sourcefiles
-  .filter((sourceFile) => !sourceFile.isDeclarationFile)
-  .forEach((sourceFile) => {
-    const fileText = sourceFile.getFullText();
-    sourceFile.forEachChild((node) => visitNode(node, checker, fileText));
-  });
+extractTypes(filePath);
